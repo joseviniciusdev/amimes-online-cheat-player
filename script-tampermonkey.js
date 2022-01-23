f083430cf3101ff9ded540d4aab40157e8f69fa3
José Vinícius <joseviniciusdev@gmail.com>
2022-01-23 16:08:45 -0300

    add filmes

--- script-tampermonkey.js
+++ script-tampermonkey.js
@@ -2,42 +2,49 @@
 // @name         Remove Protector Links animesonline.org
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://animesonline.org/episodio/*
+// @description  none
+// @author       joseviniciusdev
+// @match        https://animesonline.org/filmes/*
 // @icon         https://www.google.com/s2/favicons?domain=animesonline.org
 // @grant        none
 // ==/UserScript==
 
 $(document).ready(function(){
-        function loadPlayer(){
-            setTimeout(function(){
-                $('.source-box.on .pframe a').each(function(){
-                    let href = $(this).attr('href');
-                    let parent = $(this).parent();
+    function loadPlayer(){
+        setTimeout(function(){
+            $('.source-box.on .pframe a').each(function(){
+                let href = $(this).attr('href');
+                let parent = $(this).parent();
 
 
-                    const env = { url: href };
+                const env = { url: href };
 
-                    $.ajax({
-                        method: 'POST',
-                        url: 'https://get-player.herokuapp.com/return-player',
-                        data: JSON.stringify(env),
-                        contentType: "application/json; charset=utf-8",
-                        traditional: true,
-                        success: function(res){
-                            $('.source-box.on .pframe').html(`<iframe src="${res.player}" />`);
-                        },
-                        error: function(){
-                            console.log('error server')
-                        }
-                    })
+                $(this).parent().html('<iframe />');
 
-                });
-            },200);
-        }
+                $.ajax({
+                    method: 'POST',
+                    //url: 'https://get-player.herokuapp.com/return-player',
+                    url: 'http://localhost:3000/return-player',
+                    data: JSON.stringify(env),
+                    contentType: "application/json; charset=utf-8",
+                    traditional: true,
+                    success: function(res){
+                        $('.source-box.on .pframe').html(`<iframe src="${res.player}" allowfullscreen />`);
+                    },
+                    error: function(){
+                        console.log('error server')
+                    }
+                })
 
-      setTimeout(function(){
-        $('.dooplay_player_option').on('click', () => loadPlayer());
-      },100)
+            });
+        },200);
+    }
+
+  setTimeout(function(){
+    $('.dooplay_player_option').on('click', () => loadPlayer());
+  },100)
+
+setTimeout(function(){
+    loadPlayer();
+  },800)
 });
